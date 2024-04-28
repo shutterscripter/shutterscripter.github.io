937d5afc6f02d15623d61a2ed9626e68e51ec55c
Jayesh Shinde <68694689+shutterscripter@users.noreply.github.com>
2024-04-28 20:59:54 +0000

    changes in links for deployment

--- assets/js/projects_list.js
+++ assets/js/projects_list.js
@@ -4,7 +4,7 @@ var data = [
     description:
       "A Flutter application, a dynamic platform designed to deliver real-time news updates sourced from an open news API. The app offers users access to the latest news articles, intelligently <b>summarized</b> for quick consumption. Built upon a robust clean architecture, the app ensures seamless performance and scalability while boasting features such as search functionality, pull-to-refresh for instant updates, and the ability to save favorite articles for future reference.",
     techStack: "<b>Tech Stack :</b> Flutter, GetX, BardAPI, RoomDB, API.",
-    image: "/assets/images/temp_image.png",
+    image: "./assets/images/temp_image.png",
     link: "https://github.com/shutterscripter/FlutterPress",
   },
   {
@@ -12,7 +12,7 @@ var data = [
     description:
       "The Custom Keyboard is an Android application designed to enhance and personalize the user's typing experience.The application supports multiple key variations, including special characters and a numeric keypad, ensuring a comprehensive typing tool for all users. Additionally, the application includes sound effects for keypress feedback, adding an auditory dimension to the user's interaction with the keyboard.",
     techStack: "<b>Tech Stack :</b> Android, Java, XML, Services.",
-    image: "/assets/images/temp_image.png",
+    image: "./assets/images/temp_image.png",
     link: "https://github.com/shutterscripter/Custom_Android_Keyboard",
   },
   {
@@ -20,7 +20,7 @@ var data = [
     description:
       "DialTrack is an Android application that not only <b>manages and organizes the user's call log history</b>, but also provides a feature for <b>automatic calling via CSV files</b>. The app offers a range of features, including the ability to view and filter call logs based on various criteria such as incoming, outgoing, and missed calls. Additionally, it allows users to automate their calling process by importing contacts from CSV files. With its intuitive user interface, robust functionality, and advanced features like automatic calling.",
     techStack: "<b>Tech Stack :</b> Flutter, Firebase, Dart, GetX  ",
-    image: "/assets/images/dialtrack.jpg",
+    image: "./assets/images/dialtrack.jpg",
     link: "#",
   },
 ];
