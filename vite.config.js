aa76e13684f018b9fabb6cd86f3a185a60542710
shutterscripter <jay2272001@gmail.com>
2026-04-01 23:27:46 +0530

    Workflow added

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // GitHub Pages: serve from main branch /docs folder
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 })
